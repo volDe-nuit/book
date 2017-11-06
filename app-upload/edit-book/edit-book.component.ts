61a13e6a8bfd714bcc485e7d255fc73b0e201f0e
volDe-nuit <nachtflug_77@hotmail.com>
2017-11-06 21:21:22 +0100

    Update edit-book.component.ts

--- app-upload/edit-book/edit-book.component.ts
+++ app-upload/edit-book/edit-book.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { FirebaseService } from '../services/firebase.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Upload } from '../../models/upload';
 
 @Component({
   selector: 'app-edit-book',
@@ -9,11 +10,72 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 })
 export class EditBookComponent implements OnInit {
 
-  constructor(
-   ) { }
+$key:string;
+title:string;
+file: File;
+authors:string;
+description:string;
+category:string;
+isbn:number;
+image:string;
+imageUrl: string;
+progress: number;
+createdAt: Date = new Date();
+publisher:string;
+pages:number;
+fileChanged: boolean = false;
+/*selectedFiles: FileList;
+currentUpload: Upload; */
+
+
+  constructor(private fbs:FirebaseService,
+              private router:Router,
+              private route:ActivatedRoute
+            ) { }
 
   ngOnInit() {
+    this.$key = this.route.snapshot.params['$key'];
+
+    this.fbs.getBookDetails(this.$key).subscribe(book => {
+      this.title = book.title;
+      this.authors = book.authors;
+      this.description = book.description;
+      this.category = book.category;
+      this.isbn = book.isbn;
+      this.publisher = book.publisher;
+      this.pages = book.pages;
+      this.imageUrl = book.imageUrl;
+      this.image = book.image;
+    });
 
   }
+
+/*
+  detectFiles(event) {
+        this.selectedFiles = event.target.files;
+      }
+  uploadSingle() {
+      let file = this.selectedFiles.item(0)
+      this.currentUpload = new Upload(file);
+      this.fbs.pushUpload(this.currentUpload).then(key => {
+       this.$key = key
+       })
+     }
+*/
+
+  updateBook(){
+      let book = {
+          imageUrl: this.imageUrl,
+          title: this.title,
+          authors: this.authors,
+          description: this.description,
+          category: this.category,
+          isbn: this.isbn,
+          publisher: this.publisher,
+          pages: this.pages,
+      }
+      this.fbs.updateBook(this.$key, book);
+      this.router.navigate(['/book-list']);
+    }
 
 }
