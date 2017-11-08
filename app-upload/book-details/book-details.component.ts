92d82d5041df25255a7dd921a3e924d51beb942c
volDe-nuit <nachtflug_77@hotmail.com>
2017-11-08 22:43:51 +0100

    Update book-details.component.ts

--- app-upload/book-details/book-details.component.ts
+++ app-upload/book-details/book-details.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2/database';
 import { FirebaseService } from '../services/firebase.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 import { Router, ActivatedRoute, Params} from '@angular/router';
+import * as firebase from 'firebase';
+
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
 export class BookDetailsComponent implements OnInit {
-  upload:any;
+  book:any;
   imageUrl:any;
   $key:any;
 
-  constructor(private fireSvc:FirebaseService,
+  constructor(private fbs:FirebaseService,
+              private flashMsgeService: FlashMessagesService,
               private router:Router,
               private route:ActivatedRoute
             ) { }
@@ -21,10 +25,17 @@ export class BookDetailsComponent implements OnInit {
   ngOnInit() {
     //Get Id
     this.$key= this.route.snapshot.params['$key'];
-    this.fireSvc.getBookDetails(this.$key).subscribe(upload =>{
-      this.upload = upload;
+    this.fbs.getBookDetails(this.$key).subscribe(book =>{
+      this.book = book;
     });
-
   }
 
+deleteBook(){
+  if (confirm('Do you really want to delete book?')) {
+    this.fbs.deleteBook(this.$key);
+      this.flashMsgeService.show("Book has been successfully deleted", {cssClass: "alert-danger", timeout: 9000});
+      this.router.navigate(['/book-list']);
+    }
   }
+
+}
